938c3740e8cc768d677bf4c3bdfa48c3ab5a4678
Deepak Singh <malwareperseus@gmail.com>
2022-01-14 01:44:42 +0530

    Highlighter Complete

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import { RoughNotation, RoughNotationGroup } from 'react-rough-notation';
+import { Highlighter } from './Highlighter';
 
-export default function Hero() {
+export default function Header() {
+    const colors = ["#f59e0b", "#10b981", "#8a2be2", "#3b82f6"];
     return(
-        <p>Heeerooooo</p>
+        <div className=''>
+            <div>
+                <RoughNotationGroup show={true}>
+                    <Highlighter color={colors[0]}>
+                        <h1 className="text-4xl md:text-8xl font-bold text-gray-700 dark:text-gray-200 my-2">
+                            Developer
+                        </h1>
+                    </Highlighter>
+                    <Highlighter color={colors[1]}>
+                        <h1 className="text-4xl md:text-8xl font-bold text-gray-700 dark:text-gray-200 my-2">
+                            Speaker
+                        </h1>
+                    </Highlighter>
+                    <Highlighter color={colors[2]}>
+                        <h1 className="text-4xl md:text-8xl font-bold text-gray-700 dark:text-gray-200 my-2">
+                            Hacker
+                        </h1>
+                    </Highlighter>
+                    <Highlighter color={colors[3]}>
+                        <h1 className="text-4xl md:text-8xl font-bold text-gray-700 dark:text-gray-200 my-2">
+                            Writer
+                        </h1>
+                    </Highlighter>
+                </RoughNotationGroup>
+            </div>
+        </div>
     )
 }
