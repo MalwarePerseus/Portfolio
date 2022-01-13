e7fae135f3bedb158b61cba12f3a428910f8b945
Deepak Singh <malwareperseus@gmail.com>
2022-01-13 23:32:47 +0530

    scratch

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import ContainerBlock from '../components/ContainerBlock';
+
+export default function Home() {
+  return (
+    <p>Apple</p>
+  )
+}
