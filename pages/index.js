19fa3caabbbac2d7b89ab2e35d059bf31749f922
Deepak Singh <malwareperseus@gmail.com>
2022-01-14 00:52:52 +0530

    Imported Header

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import ContainerBlock from '../components/ContainerBlock';
-
+import Header from '../components/Header'
 export default function Home() {
   return (
     <ContainerBlock
@@ -9,6 +9,7 @@ export default function Home() {
       description="Deepak Singh is a software developer, designer, writer, speaker, and entrepreneur. Also known as MalwarePerseus, he is known for his CTF and Cyber Hunt wins."
     >
 
+      <Header/>
     </ContainerBlock>  
   );
 }
