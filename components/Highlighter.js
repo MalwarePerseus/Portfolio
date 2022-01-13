3da63edee8c2288557d5277485309c9c1c9529ee
Deepak Singh <malwareperseus@gmail.com>
2022-01-14 01:32:39 +0530

    Rough Notion based Highlighting Component

--- components/Highlighter.js
+++ components/Highlighter.js
@@ -1,5 +1,19 @@
 import React from 'react';
+import { RoughNotation } from 'react-rough-notation';
 
-export default function Highlighter(){
+export const Highlighter = ({color, children}) => {
+    const animationDuration = Math.floor(30 * children.length)
     
-}
+    return(
+        <RoughNotation
+            type='highlight'
+            color={color}
+            multiline={true}
+            padding={[0,2]}
+            iterations={1}
+            animationDuration={animationDuration}
+        >
+            {children}
+        </RoughNotation>
+    );
+};
