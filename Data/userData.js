9074d7f44757b8679ee86d4aa7d3ecbc0029f375
Deepak Singh <malwareperseus@gmail.com>
2022-01-13 21:09:37 +0530

    Added User Details

---
+++ Data/userData.js
@@ -0,0 +1,53 @@
+const userData = {
+    githubUsername: "MalwarePerseus",
+    name: "Deepak Singh",
+    designation: "Full-Stack Developer",
+    avatarUrl: "/avatar.png",
+    email: "malwareperseus@gmail.com",
+    phone: "+91 9793957136",
+    address: "New Delhi, India.",
+    projects: [
+      {
+        title: "Project",
+        link: "link",
+        imgUrl: "/img.jpg",
+      },
+    ],
+    about: {
+      title:
+        "Something about me",
+      description: [
+        `Good words in detail`
+        ],
+      currentProject: "This Porfolio",
+      currentProjectUrl: "https://malwareperseus.vercel.app",
+    },
+    experience: [
+      {
+        title: "Student",
+        company: "Army Public School, Delhi cantt",
+        year: "2021-22",
+        companyLink: "apsdelhicannt.com",
+        desc: "Current Headboy of the school",
+      },
+      {
+        title: "Founder and Teacher",
+        company: "Int_Elligence",
+        year: "2021",
+        companyLink: "https://google.com",
+        desc: "School Club where i teach stuff",
+      },
+      
+    ],
+    resumeUrl:
+      "https://google.com",
+    socialLinks: {
+      instagram: "https://instagram.com/malware_perseus",
+      twitter: "https://twitter.com/malwareperseus",
+      linkedin: "https://linkedin.com/",
+      github: "https://github.com/MalwarePerseus",
+      facebook: "https://facebook.com/",
+    },
+  };
+  
+  export default userData;
